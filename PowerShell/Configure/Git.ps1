9178d222bb41647bcdc3440d7b6da5567c3fd181
Jared Parsons <jaredpparsons@gmail.com>
2009-11-28 20:47:28 -0800

    Added Color support to Git by default

--- PowerShell/Configure/Git.ps1
+++ PowerShell/Configure/Git.ps1
@@ -4,5 +4,11 @@ git config --global user.name "Jared Parsons"
 git config --global user.email "jaredpparsons@gmail.com"
 git config --global core.autocrlf false
 
+# Colors are pretty 
+git config --global color.branch always
+git config --global color.showbranch always
+git config --global color.status always
+
+
 $file = join-path $Jsh.ConfigPath Data\.gitignore
 git config --global core.excludesfile "$file"
