f30d07e86a2a45fa9caa6498a17e88691d9a0970
Jared Parsons <jaredpparsons@gmail.com>
2009-11-30 20:36:28 -0800

    Removed some other unneeded stuff

--- Scripts/InitMachine/Deploy.ps1
+++
@@ -1,8 +0,0 @@
-$initPath = split-path -parent $MyInvocation.MyCommand.Definition 
-$target = "\\vbqafiles\public\jaredpar\InitMachine"
-if ( -not (test-path $target) ) {
-    mkdir $target
-}
-
-copy -r -force -exclude ".svn","Deploy.ps1" * $target
-copy -force $Jsh.ScriptMap["common"] $target
