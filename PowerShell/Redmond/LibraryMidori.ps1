468ea23e27a766dd89d6c46b6a8013d60c41e12e
Jared Parsons <jaredpparsons@gmail.com>
2010-06-29 14:00:06 -0700

    Updated the midori script

--- PowerShell/Redmond/LibraryMidori.ps1
+++ PowerShell/Redmond/LibraryMidori.ps1
@@ -1,10 +1,4 @@
 
-if ( -not (test-path env:\MIDROOT) ) {
-    write-error "Needs to be run under the midori environment"
-    exit
-}
 
-$midPath = ${env:MIDROOT}
-$winPath = join-path $midPath "Internal\Bin\Windows"
+set-alias sd \\ptt\Release\SD\Current\X86\sd.exe 
 
-set-alias jjpack (join-path $winPath "jjpack.exe") 
