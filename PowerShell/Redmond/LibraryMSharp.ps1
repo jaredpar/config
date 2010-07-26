0b6c1938a96498893ced3b45af38057f6ed3de49
Jared Parsons <jaredpparsons@gmail.com>
2010-07-26 13:49:48 -0700

    Updated the build pulling script

--- PowerShell/Redmond/LibraryMSharp.ps1
+++ PowerShell/Redmond/LibraryMSharp.ps1
@@ -1,4 +1,25 @@
 
+function Get-MidoriBuild() {
+    param ( $branch = "framework",
+            [switch]$built = $true )
+    $sourcePath = join-path "e:\dd\midori\branches" $branch
+    if ( $built ) {
+        $sourcePath = join-path $sourcePath "Midori.obj\Windows\x86.Debug"
+    }
+    else {
+        $sourcePath = join-path "e:\dd\midori\branches" $branch
+        $sourcePath = join-path $sourcePath "Midori.obj\CIBTools"
+    }
+    $devPath = (join-path $env:userprofile "Midori")
+    pushd (join-path $env:DepotRoot "midori\assemblies")
+    tf edit *
+    foreach ( $i in gci *) {
+        copy (join-path $sourcePath $i.Name) .
+        copy $i.FullName $devPath
+    }
+    popd
+}
+
 if ( test-path env:\DepotRoot ) {
     set-alias updatebin (join-path $env:DepotRoot "midori\build\scripts\updatebinaries.cmd")
 }
