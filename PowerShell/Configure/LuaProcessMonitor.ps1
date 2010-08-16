3e7f5cc003a7d37902180e158b7f84aa36957765
Jared Parsons <jaredpparsons@gmail.com>
2010-08-16 12:45:15 -0700

    Removed the LuaProcessMonitor code

--- PowerShell/Configure/LuaProcessMonitor.ps1
+++ PowerShell/Configure/LuaProcessMonitor.ps1
@@ -1,15 +1,9 @@
 
-if ( $Jsh.IsTestMachine ) {
-    return
+# Remove the previous LUA monitor program
+echo "Removing LuaProcessMonitor"
+$proc = gps LuaProcessMonitor -ErrorAction SilentlyContinue
+if ( $proc -ne $null ) {
+    kill $proc
 }
 
-# Set LUA process monitor to run when we login
-echo "Making LuaProcessMonitor start on login"
-$luaExe = join-path $Jsh.UtilsRawPath "LuaProcessMonitor.exe"
-sp hkcu:\Software\Microsoft\Windows\CurrentVersion\Run LuaProcessMonitor $luaExe
-
-$found = @( $(gps LuaProcessMonitor -ea SilentlyContinue))
-if ( $found.Length -eq 0 )
-{
-    ii $luaExe
-}
+rp hkcu:\Software\Microsoft\Windows\CurrentVersion\Run LuaProcessMonitor -ErrorAction SilentlyContinue
