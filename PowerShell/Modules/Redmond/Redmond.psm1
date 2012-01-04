f772dd0cf4a0a3e277ff0adfe757ac0ff3427fca
Jared Parsons <jaredpparsons@gmail.com>
2012-01-04 08:50:07 -0800

    Make starting M# easier

--- PowerShell/Modules/Redmond/Redmond.psm1
+++ PowerShell/Modules/Redmond/Redmond.psm1
@@ -1,55 +1,34 @@
 
-function Get-Razzle() {
-    param ( [string]$name="*" )
-
-    foreach ( $letter in Get-LocalDrive ) {
-        $ddPath = join-path $letter "dd"
-        write-debug "Considering: $ddPath"
-        if ( test-path $ddPath ) {
-            foreach ( $cur in gcid $ddPath ) {
-                if ( -not ( $cur.Name -like $name) ) {
-                    continue
-                }
-
-                $depotPath = join-path $cur.FullName "src"
-                write-debug "Considering: $depotPath"
-                if ( test-path $depotPath ) {
-                    write-output (New-Tuple "Name",$cur.Name,"DepotPath",$depotPath)
-                }
-            }
-        }
-    }
-}
-
 # Method to startup a PowerShell razzle environment
-function Set-Razzle() {
-    param ( [string]$name = $(throw "Provide a name in the map"),
-            [string]$arch = "x86",
+function Set-MSharp() {
+    param ( [string]$branch = $(throw "Pick a branch"),
             [string]$flavor = "chk" ) 
 
     # Razzle won't run in a 64 bit shell or a non-admin
     if ( (test-win64) -or (-not (test-admin))) {
         write-host "Starting 32 bit admin shell"
-        Invoke-CommandAdmin "set-razzle $name $arch $flavor " -dotsource -use32
+        Invoke-CommandAdmin "set-msharp $branch $flavor " -dotsource -use32
         return
     }
 
-    $info = get-razzle $name
-    if ( $info -eq $null )
-    {
+    $path = join-path 'e:\dd\midori\branches' $branch
+    $path = join-path $path 'MSharp'
+    if ( -not (test-path $path)) {
         write-output "Error: $name is not a valid razzle entry"
         get-razzle
         return
     }
 
     # Build up the razzle arguments.  Don't run OACR unless we are in RET
-    $razzleArgs = "{0} {1}" -f $arch,$flavor
+    $razzleArgs = $flavor
     if ( $flavor -ne "ret" ) {
         $razzleArgs += " No_OACR No_opt"
     }
 
     # Actually start razzle
-    . (join-path $info.DepotPath "tools\razzle.ps1") $razzleArgs
+    . (join-path $path "tools\razzle.ps1") $razzleArgs
+
+    import-module MSharp -Global
 }	
 
 # Method to startup a Midori environment
@@ -65,7 +44,7 @@ function Set-Midori() {
     cd $path
     cd Midori
     . .\setenv.ps1
-    import-module Midori
+    import-module Midori -Global
     . set-env 
 }	
 
@@ -84,8 +63,7 @@ function Disable-StrongName() {
     & $sn64 -Vr *
 }
 
-function Set-Midori()
-{
+function Set-Midori() {
     param ( [string]$branch = 'framework')
 
     $path = join-path 'e:\dd\midori\branches' $branch
