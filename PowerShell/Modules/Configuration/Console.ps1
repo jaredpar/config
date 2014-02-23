28a77c4f24adbca0eaa1a55746cc84c01932bc9f
Jared Parsons <jaredpar@microsoft.com>
2014-02-22 18:21:37 -0800

    Removed unneeded configuration
    Removed some more configuration elements that aren't needed anymore
    since I have chocolatey

--- PowerShell/Modules/Configuration/Console.ps1
+++
@@ -1,7 +0,0 @@
-
-# Copy Console.exe to the desktop directory and pin it 
-
-$dest = join-path (join-path $env:UserProfile "Desktop") "Console"
-$source = join-path $PSScriptRoot "Console" 
-& $PSScriptRoot\SmartDeployDirectory.ps1 $source $dest 
-& $PSScriptRoot\PinToTaskbar.ps1 (join-path $dest "Console.exe")
