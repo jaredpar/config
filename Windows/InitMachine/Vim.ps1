7c04344e065bb7decaa69f11a76a884351ab6a29
Jared Parsons <jaredpar@microsoft.com>
2015-01-11 18:27:13 -0800

    Fixed the vim scripts

--- Windows/InitMachine/Vim.ps1
+++ Windows/InitMachine/Vim.ps1
@@ -1,14 +1,21 @@
 
 # Add the _vimrc file to the %HOME% path which just calls the real 
 # one I have in data\vim
-echo "Generating a _vimrc"
 $dataDir = join-path (split-path -parent (split-path -parent $PSScriptRoot)) "Data"
-$vimDir = join-path $dataDir "vim"
-$realPath = resolve-path (join-path $vimDir "_vimrc")
-$vimrcPath = join-path $env:UserProfile "_vimrc"
-echo ":source $realPath" | out-file -encoding ASCII $vimrcPath
+
+# Generate _vimrc which calls the real one 
+echo "Generating a _vimrc"
+$realPath = resolve-path (join-path $dataDir "_vimrc")
+$sourcePath = join-path $env:UserProfile "_vimrc"
+echo ":source $realPath" | out-file -encoding ASCII $sourcePath
+
+# Generate _vsvimrc which calls the real one 
+echo "Generating a _vsvimrc"
+$realPath = resolve-path (join-path $dataDir "_vsvimrc")
+$sourcePath = join-path $env:UserProfile "_vsvimrc"
+echo ":source $realPath" | out-file -encoding ASCII $sourcePath
 
 echo "Copying VimFiles" 
-$source = join-path $vimDir "vimfiles"
+$source = join-path $dataDir "vim\vimfiles"
 copy -re -fo $source $env:UserProfile
 
