478b0857f50c21d95c77f7c8423f0e15de3d9e7d
Jared Parsons <jaredpparsons@gmail.com>
2015-04-14 19:59:01 -0700

    Fixed the linux init script

--- Linux/init.sh
+++ Linux/init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-ln -s ~/Documents/config/Linux/.vimrc ~/.vimrc
+ln -sf ~/Documents/config/Linux/.vimrc ~/.vimrc
 
+if [ -L ~/.vim ]; then
+    rm ~/.vim
+fi
+ln -sf ~/Documents/config/Linux/vimfiles ~/.vim
+
+git config --global user.email "jaredpparsons@gmail.com"
+git config --global user.name "Jared Parsons"
