9e911f928af6060b2d6ce05feaea9d79ee5fb54e
Jared Parsons <jaredpparsons@gmail.com>
2015-06-08 14:32:32 -0700

    Update inputrc

--- Mac/init.sh
+++ Mac/init.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 
 ln -sf ~/Documents/config/Mac/.vimrc ~/.vimrc
+cp ~/Documents/config/Mac/.inputrc ~/.inputrc
 
 git config --global user.email "jaredpparsons@gmail.com"
 git config --global user.name "Jared Parsons"
