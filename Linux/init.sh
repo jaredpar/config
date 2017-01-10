ba4b7fe42fd5c5499c20b9e7823010b1cd150382
Jared Parsons <jaredpparsons@gmail.com>
2017-01-10 08:39:36 -0800

    Remove hard coded directory

--- Linux/init.sh
+++ Linux/init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ln -sf ~/Documents/config/Linux/.vimrc ~/.vimrc
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sf $THIS_DIR/.vimrc ~/.vimrc
 
 if [ -L ~/.vim ]; then
     rm ~/.vim
 fi
-ln -sf ~/Documents/config/Linux/vimfiles ~/.vim
+ln -sf $THIS_DIR/vimfiles ~/.vim
 
 git config --global user.email "jaredpparsons@gmail.com"
 git config --global user.name "Jared Parsons"
