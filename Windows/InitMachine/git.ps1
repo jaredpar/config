aadc718bc0dace49b9fa0d9d5d4a4d6412e7aee6
Jared Parsons <jaredpar@microsoft.com>
2017-02-22 08:18:13 -0800

    Fix the config

--- Windows/InitMachine/git.ps1
+++ Windows/InitMachine/git.ps1
@@ -1,2 +1,4 @@
 
-git config --global core.editor "C:\Program Files (x86)\Vim\vim74\vim.exe"
+git config --global core.editor "C:\Program Files (x86)\Vim\vim80\vim.exe"
+git config --global user.name "Jared Parsons"
+git config --global user.email "jaredpar@microsoft.com"
