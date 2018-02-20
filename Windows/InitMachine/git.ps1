ba183b2d3bd9af167ae535067f775bcfe1e54065
Jared Parsons <jaredpparsons@gmail.com>
2018-02-20 15:44:32 -0800

    Enable signing setup

--- Windows/InitMachine/git.ps1
+++ Windows/InitMachine/git.ps1
@@ -1,4 +1,12 @@
 
-git config --global core.editor "C:\Program Files (x86)\Vim\vim80\vim.exe"
-git config --global user.name "Jared Parsons"
-git config --global user.email "jaredpparsons@gmail.com"
+
+# Setup the standard user info.
+& git config --global core.editor "C:\Program Files (x86)\Vim\vim80\vim.exe"
+& git config --global user.name "Jared Parsons"
+& git config --global user.email "jaredpparsons@gmail.com"
+
+# Setup signing policy.
+& git config --global gpg.program "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
+& git config --global commit.gpgsign true
+& git config --global user.signkey 06EDAA3E3C0AF8841559
+
