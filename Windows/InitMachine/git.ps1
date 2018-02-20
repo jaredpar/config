beadca9bce7e5effa8b06f2da2cec13c54ecbf89
Jared Parsons <jaredpparsons@gmail.com>
2018-02-20 15:59:57 -0800

    Proper escape of vim

--- Windows/InitMachine/git.ps1
+++ Windows/InitMachine/git.ps1
@@ -1,7 +1,7 @@
 
 
 # Setup the standard user info.
-& git config --global core.editor "C:\Program Files (x86)\Vim\vim80\vim.exe"
+& git config --global core.editor "'C:\Program Files (x86)\Vim\vim80\vim.exe'"
 & git config --global user.name "Jared Parsons"
 & git config --global user.email "jaredpparsons@gmail.com"
 
