de008315ad320726fb6298dd791c01bb6e2571a4
Jared Parsons <jaredpparsons@gmail.com>
2018-06-27 05:20:42 +0000

    Update init

--- Linux/init.sh
+++ Linux/init.sh
@@ -10,3 +10,6 @@ ln -sf $THIS_DIR/vimfiles ~/.vim
 
 git config --global user.email "jaredpparsons@gmail.com"
 git config --global user.name "Jared Parsons"
+git config --global user.signkey 58B3065D
+git config --global commit.gpgsign true
+git config --global core.editor "vim"
