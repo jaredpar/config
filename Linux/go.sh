488e24f5ddaad6a670e461bb49fc022d549a33e7
Jared Parsons <jaredpparsons@gmail.com>
2020-08-05 11:06:34 -0700

    Git settings for Linux

--- Linux/go.sh
+++ Linux/go.sh
@@ -12,3 +12,6 @@ ln -sf $THIS_DIR/.bashrc ~/.bashrc
 git config --global user.email "jaredpparsons@gmail.com"
 git config --global user.name "Jared Parsons"
 git config --global core.editor "vim"
+git config --global fetch.prune true
+git config --global push.default current
+git config --global commit.gpgsign false
