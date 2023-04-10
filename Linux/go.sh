61b42acba9740898731d723d8c71631c3fd56a24
Jared Parsons <jared@popcornbear.org>
2023-04-10 15:39:43 -0700

    Clean up the linux path

--- Linux/go.sh
+++ Linux/go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENT_DIR=$(realpath "$THIS_DIR/..")
 ln -sf $THIS_DIR/.vimrc ~/.vimrc
 
 if [ -L ~/.vim ]; then
@@ -8,5 +9,5 @@ if [ -L ~/.vim ]; then
 fi
 ln -sf $THIS_DIR/vimfiles ~/.vim
 ln -sf $THIS_DIR/.bashrc ~/.bashrc
-ln -sf $THIS_DIR/../CommonData/.gitconfig  ~/.gitconfig
+ln -sf $PARENT_DIR/CommonData/.gitconfig  ~/.gitconfig
 
