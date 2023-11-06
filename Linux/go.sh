6994909f94490fad5b5b468555dbabaa02090e30
Jared Parsons <jared@paranoidcoding.org>
2023-11-06 15:10:03 -0800

    Fix up Linux

--- Linux/go.sh
+++ Linux/go.sh
@@ -15,12 +15,23 @@ fi
 echo "source $THIS_DIR/.bashrc $THIS_DIR" > ~/.bashrc
 
 ln -sf $THIS_DIR/vimfiles ~/.vim
-ln -sf $PARENT_DIR/CommonData/.gitconfig  ~/.gitconfig
+
+echo "Configure Git"
+git config --global user.name "Jared Parsons"
+git config --global user.email jared@paranoidcoding.org
+git config --global fetch.prune true
+git config --global core.longpaths true
+git config --global push.default current
+git config --global commit.gpgsign false
+git config --global alias.assume 'update-index --assume-unchanged'
+git config --global alias.unassume 'update-index --no-assume-unchanged'
+git config --global core.editor vim
+
 mkdir -p "$THIS_DIR/Local"
 
 BASHRC_LOCAL="$THIS_DIR/Local/bashrc-local.sh"
 if [ ! -f "$BASHRC_LOCAL" ]; then
     echo "# Machine specific bash goes here" > $BASHRC_LOCAL
-    chhmod +x $BASHRC_LOCAL
+    chmod +x $BASHRC_LOCAL
 fi
 
