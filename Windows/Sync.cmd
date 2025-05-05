79c01ada5052379a88227ccd7a0868b6821dff3e
Jared Parsons <jared@paranoidcoding.org>
2025-05-05 15:21:07 -0700

    Fix issues

---
+++ Windows/Sync.cmd
@@ -0,0 +1,6 @@
+@echo off
+set SOURCE=%~dp0
+
+REM Used the -ExecutionPolicy switch because it's likely that
+REM execution of scripts is not enabled yet
+pwsh -NoProfile -ExecutionPolicy RemoteSigned %SOURCE%\Sync.ps1 %*
