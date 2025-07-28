57931377cfc154e87f6383d157e122c83c56314a
Jared Parsons <jared@paranoidcoding.org>
2025-07-28 09:39:27 -0700

    App install script

---
+++ Windows/Apps.ps1
@@ -0,0 +1,34 @@
+
+Set-StrictMode -version 2.0
+$ErrorActionPreference = "Stop"
+
+function Install-App([string]$appName, [string]$id) {
+
+  Write-Host "Installing $appName with id $id"
+  $t = Exec-CommandRaw "winget" "list --id $id"
+  if ($t.ExitCode -eq 0) {
+    Write-Host "$appName is already installed"
+    return
+  }
+
+  Exec-Console "winget" "install --id $id"
+}
+
+try {
+  . (Join-Path $PSScriptRoot "Common-Utils.ps1")
+  Push-Location $PSScriptRoot
+
+  Install-App "ILSpy" "icsharpcode.ILSpy"
+  Install-App "Vim" "vim.vim"
+
+  exit 0
+}
+catch {
+  Write-Host $_
+  Write-Host $_.Exception
+  Write-Host $_.ScriptStackTrace
+  exit 1
+}
+finally {
+  Pop-Location
+}
